b3f3d529977e6463065d2165be24d3c574ced4b9
Wesley <wesleyTavaresDev@outlook.com>
2022-01-26 15:51:38 -0300

    Factorial number system ready

---
+++ src/src/FactorialNumber.java
@@ -0,0 +1,17 @@
+public class FactorialNumber
+{
+    public static void main(String[] args)
+    {
+        FactorialNumber fact  = new FactorialNumber();
+
+        System.out.println(fact.Factorial(3));
+    }
+
+    int Factorial(int number)
+    {
+        if(number <= 1)
+            return 1;
+        else
+            return number * Factorial(number - 1);
+    }
+}
